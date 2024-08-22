4e3c0438131c82b7f11acb6e7893af5cf0e6ef08
sowrabhasg <147090377+sowrabhasg@users.noreply.github.com>
2024-08-22 16:07:40 +0530

    5th commit

---
+++ RockAndRollGit/src/test/java/github/Samplae.java
@@ -0,0 +1,13 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Samplae
+{
+	@Test
+	public void sample123()
+	{
+		Reporter.log("hello india");
+	}
+}
